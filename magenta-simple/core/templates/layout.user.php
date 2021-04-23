e21affe033df27503c55aeaf9354f57faad323eb
JAIN <dipak.jain@t-systems.com>
2021-04-23 17:49:21 +0530

    Added test theme

--- magenta-simple/core/templates/layout.user.php
+++ magenta-simple/core/templates/layout.user.php
@@ -86,8 +86,8 @@
 		<div id="notification-container">
 			<div id="notification"></div>
 		</div>
-		<header role="banner" id="header">
-			<div class="brandbar">
+
+		<div class="brandbar">
 				<div class="container-fixed">
 					<div class="brand-logo">
 						<img src="deutsche-telekom-logo.svg" alt="Telekom Logo">
@@ -99,7 +99,8 @@
 					</div>
 				</div>
 			</div>
-		
+
+		<header role="banner" id="header">
 			<div class="header-left">
 				<a href="<?php print_unescaped(link_to('', 'index.php')); ?>"
 					id="nextcloud">
