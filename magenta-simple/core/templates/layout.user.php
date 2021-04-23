4bb9339960b85f9048ab4475e50932620e54a2e1
JAIN <dipak.jain@t-systems.com>
2021-04-23 18:34:35 +0530

    Added test theme

--- magenta-simple/core/templates/layout.user.php
+++ magenta-simple/core/templates/layout.user.php
@@ -57,12 +57,13 @@
 			}
 
 			function myFunction() {
-				if (document.documentElement.scrollTop > 50) {
+				debugger;
+				if (document.documentElement.scrollTop > 80) {
 					document.getElementsByClassName("brandbar")[0].style.height = "5px";
 					document.getElementsByClassName("brandbar")[0].style.position = "fixed";
 					document.getElementsByClassName("container-fixed")[0].style.display = "none";
 				}
-				else if (document.documentElement.scrollTop < 50) {
+				else if (document.documentElement.scrollTop < 80) {
 					document.getElementsByClassName("brandbar")[0].style.height = "auto";
 					document.getElementsByClassName("brandbar")[0].style.position = "static";
 					document.getElementsByClassName("container-fixed")[0].style.display = "block";
