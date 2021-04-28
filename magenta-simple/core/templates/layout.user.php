6fa38cf0ee72abd88f8692a3a6961e3d82eb5a05
JAIN <dipak.jain@t-systems.com>
2021-04-28 14:54:51 +0530

    Added test theme

--- magenta-simple/core/templates/layout.user.php
+++ magenta-simple/core/templates/layout.user.php
@@ -48,20 +48,28 @@
 
     /* Brand Footer */
 
-    @media (min-width: 1200px) .container-fixed {
-        max-width: 1140px;
+    @media screen and (min-width: 1200px) .container-fixed {
+        .container-fixed {
+            max-width: 1140px;
+        }
     }
 
-    @media (min-width: 992px) .container-fixed {
-        max-width: 960px;
+    @media screen and (min-width: 992px) .container-fixed {
+        .container-fixed {
+            max-width: 960px;
+        }
     }
 
-    @media (min-width: 768px) .container-fixed {
-        max-width: 720px;
+    @media screen and (min-width: 768px) .container-fixed {
+        .container-fixed {
+            max-width: 720px;
+        }
     }
 
-    @media (min-width: 576px) .container-fixed {
-        max-width: 540px;
+    @media screen and (min-width: 576px) .container-fixed {
+        .container-fixed {
+            max-width: 540px;
+        }
     }
 
     .container-fixed {
