e24f9dc3bf0ea6924a64248663cc34a06c8d3319
EMEA2\A94860742 <mankarandeep.saini@t-systems.com>
2021-05-20 11:15:24 +0530

    Adjusted width for breadcrumb

--- magenta-simple/core/templates/layout.user.php
+++ magenta-simple/core/templates/layout.user.php
@@ -193,8 +193,8 @@
       </header> 
     </div>
     <div class="breadcrumb-bar">
-        <div class="col-sm-6 left-part"></div>
-        <div class="col-sm-6 right-part">
+        <div class="left-part" style="width=:410px;"></div>
+        <div class="right-part" size="width= calc(100% - 410px);">
         <ul class="breadcrumb-style">
                 <li><a><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 16" height="16" width="16">
                             <path d="m8 1l-8 8h3v6h10v-6h3l-3-3v-4h-3v1l-2-2z" />
