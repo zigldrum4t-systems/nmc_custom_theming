76bc6de27e6514da578fdee548ae0d93f0303cb0
EMEA2\A94860742 <mankarandeep.saini@t-systems.com>
2021-05-20 11:40:26 +0530

    Added column structure for breadcrumb

--- magenta-simple/core/templates/layout.user.php
+++ magenta-simple/core/templates/layout.user.php
@@ -194,8 +194,8 @@
     </div>
     <div class="breadcrumb-bar">
         <div class="container-fixed">
-        <div class="left-part" style="width=:410px;"></div>
-        <div class="right-part" size="width= calc(100% - 410px);">
+        <div class="col-xs-3 left-part"></div>
+        <div class="col-xs-9 right-part">
         <ul class="breadcrumb-style">
                 <li><a><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 16" height="16" width="16">
                             <path d="m8 1l-8 8h3v6h10v-6h3l-3-3v-4h-3v1l-2-2z" />
