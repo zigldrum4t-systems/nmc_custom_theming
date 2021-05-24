1ca00a3154da423454572d425ed20076f1901c20
EMEA2\A94860742 <mankarandeep.saini@t-systems.com>
2021-05-24 10:34:19 +0530

    Tested icon code-MANKARAN

--- magenta-simple/core/templates/layout.user.php
+++ magenta-simple/core/templates/layout.user.php
@@ -136,9 +136,10 @@
             <div class="header-right">
 
                <div class="navbar-icons">
-                 <a href="#"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 16" height="16" width="16">
+               <a href="#"><scale-icon-action-search accessibility-title="search" /><label>Search</label></a>
+                 <!-- <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 16" height="16" width="16">
                             <path d="m8 1l-8 8h3v6h10v-6h3l-3-3v-4h-3v1l-2-2z" />
-                        </svg><label>Search</label></a>
+                        </svg> -->
                  <a href="#"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 16" height="16" width="16">
                             <path d="m8 1l-8 8h3v6h10v-6h3l-3-3v-4h-3v1l-2-2z" />
                         </svg><label>Mail</label></a>
