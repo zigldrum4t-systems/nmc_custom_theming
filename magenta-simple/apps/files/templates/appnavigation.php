64fc79be7dd77a8a50d46b69fda1c1970628aed0
JAIN <dipak.jain@t-systems.com>
2021-05-25 17:38:29 +0530

    Added test theme - Dipak

--- magenta-simple/apps/files/templates/appnavigation.php
+++ magenta-simple/apps/files/templates/appnavigation.php
@@ -1,7 +1,7 @@
 <?php
 script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
 ?>
-<div id="app-navigation">
+<div id="app-navigation" style="background: red">
 	<ul class="with-icon">
 
 		<?php
