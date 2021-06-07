12a78f90ea845402e90b36a0eae08335bad71596
Desai <Sangramsinh.Desai@t-systems.com>
2021-06-07 17:33:27 +0530

    Reverted changes for my shares as not taking effect-Sangram Desai

--- magenta-simple/apps/files/templates/appnavigation.php
+++ magenta-simple/apps/files/templates/appnavigation.php
@@ -9,18 +9,18 @@ script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
 		$customNavigationItems['favorites']['order']=0;
 		$customNavigationItems['files'] = $_['navigationItems']['files'];
 		$customNavigationItems['files']['order']=1;
-		/***  My shares **/
-		$customNavigationItems['my_shares'] = array(
+		
+		/*$customNavigationItems['my_shares'] = array(
 			"id" = "my_shares","appname" => "files","script" => "","order"=>2,
 			"name"=>"My shares", "active"=>"","icon"=>"","classes"=>"","type"=>"link"
 		);
-		
 
-		/***  Shared with me **/
+
+		
 		$customNavigationItems['shared_with_me'] = array(
 			"id" = "shared_with_me","appname" => "files","script" => "","order"=>3,
 			"name"=>"Shared with me", "active"=>"","icon"=>"","classes"=>"","type"=>"link"
-		);
+		);*/
 
 		$customNavigationItems['trashbin'] = $_['navigationItems']['trashbin'];
 		$pinned = 0;
