b7a3c298cef09d2e2d477eb1c2230029c7821475
JAIN <dipak.jain@t-systems.com>
2021-06-08 13:38:22 +0530

    Reverted 3 checkins of Mankarandeep - Dipak

--- magenta-simple/apps/files/templates/appnavigation.php
+++ magenta-simple/apps/files/templates/appnavigation.php
@@ -1,9 +1,9 @@
 <?php
 script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
-?>	
+?>
 <div id="app-navigation">
-  <ul class="with-icon">
-		<?php
+    <ul class="with-icon">
+        <?php
 		$customNavigationItems['favorites'] = $_['navigationItems']['favorites'];
 		$customNavigationItems['files'] = $_['navigationItems']['files'];
 
@@ -24,65 +24,67 @@ script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
 		 }		
 	?>
 
-  </ul>
+    </ul>
 
     <div class="memoryused">
-		<?php if ($_['quota'] === \OCP\Files\FileInfo::SPACE_UNLIMITED): ?>
-				<div id="quota" class="pinned <?php p($pinned === 0 ? 'first-pinned ' : '') ?>">
-					<a href="#" class="icon-image">
-						<img src='C:/Magenta cloud/themes/custom-theme/core/img/favicon.png'>
-						<p class="memorytext"><?php p($l->t('%s used', [$_['usage']])); ?></p>
-					</a>
-				</div>
-			<?php else: ?>
-				<div id="quota" class="has-tooltip pinned <?php p($pinned === 0 ? 'first-pinned ' : '') ?>"
-					title="<?php p($l->t('%s%% of %s used', [$_['usage_relative'], $_['total_space']])); ?>">
-					<a href="#" class="icon-quota svg">
-						<p id="quotatext"><?php p($l->t('%1$s of %2$s used', [$_['usage'], $_['total_space']])); ?></p>
-						<div class="quota-container">
-							<progress value="<?php p($_['usage_relative']); ?>" max="100" class="<?= ($_['usage_relative'] > 80) ? 'warn' : '' ?>"></progress>
-						</div>
-					</a>
-				</div>
-			<?php endif; ?>
-	</div>
+        <?php if ($_['quota'] === \OCP\Files\FileInfo::SPACE_UNLIMITED): ?>
+        <div id="quota" class="pinned <?php p($pinned === 0 ? 'first-pinned ' : '') ?>">
+            <a href="#" class="icon-image">
+                <img src='C:/Magenta cloud/themes/custom-theme/core/img/favicon.png'>
+                <p class="memorytext"><?php p($l->t('%s used', [$_['usage']])); ?></p>
+            </a>
+        </div>
+        <?php else: ?>
+        <div id="quota" class="has-tooltip pinned <?php p($pinned === 0 ? 'first-pinned ' : '') ?>"
+            title="<?php p($l->t('%s%% of %s used', [$_['usage_relative'], $_['total_space']])); ?>">
+            <a href="#" class="icon-quota svg">
+                <p id="quotatext"><?php p($l->t('%1$s of %2$s used', [$_['usage'], $_['total_space']])); ?></p>
+                <div class="quota-container">
+                    <progress value="<?php p($_['usage_relative']); ?>" max="100"
+                        class="<?= ($_['usage_relative'] > 80) ? 'warn' : '' ?>"></progress>
+                </div>
+            </a>
+        </div>
+        <?php endif; ?>
+    </div>
 
 
-	<div class="NextCloudPorgressBar">
-	  <div class="progress customprogressbar">
-       <div class="progress-bar styledbar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-       <span class="sr-only">60% Complete</span>
-      </div>
-	</div>
-	
+    <div class="NextCloudPorgressBar">
+        <div class="progress customprogressbar">
+            <div class="progress-bar styledbar" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                aria-valuemax="100" style="width: 60%;">
+                <span class="sr-only">60% Complete</span>
+            </div>
+        </div>
+
+
+        <!-- <li><span class="bar"><span class="style-html"></span></span></li> -->
+    </div>
     <div class="custom-button">
-	  <button type="button" class="btn btn-default btn-xs btn-style ">Expand Storage</button>
-	</div>
-	<div id="app-settings">
-		<div id="app-settings-header">
-			<button class="settings-button"
-					data-apps-slide-toggle="#app-settings-content">
-				<?php p($l->t('Settings')); ?>
-			</button>
-		</div>
-		<div id="app-settings-content">
-			<div id="files-app-settings"></div>
-			<div id="files-setting-showhidden">
-				<input class="checkbox" id="showhiddenfilesToggle"
-					   checked="checked" type="checkbox">
-				<label for="showhiddenfilesToggle"><?php p($l->t('Show hidden files')); ?></label>
-			</div>
-			<div id="files-setting-cropimagepreviews">
-				<input class="checkbox" id="cropimagepreviewsToggle"
-					   checked="checked" type="checkbox">
-				<label for="cropimagepreviewsToggle"><?php p($l->t('Crop image previews')); ?></label>
-			</div>
-			<!-- <label for="webdavurl"><?php p($l->t('WebDAV')); ?></label>
+        <button type="button" class="btn btn-default btn-style">Expand Storage</button>
+    </div>
+    <div id="app-settings">
+        <div id="app-settings-header">
+            <button class="settings-button" data-apps-slide-toggle="#app-settings-content">
+                <?php p($l->t('Settings')); ?>
+            </button>
+        </div>
+        <div id="app-settings-content">
+            <div id="files-app-settings"></div>
+            <div id="files-setting-showhidden">
+                <input class="checkbox" id="showhiddenfilesToggle" checked="checked" type="checkbox">
+                <label for="showhiddenfilesToggle"><?php p($l->t('Show hidden files')); ?></label>
+            </div>
+            <div id="files-setting-cropimagepreviews">
+                <input class="checkbox" id="cropimagepreviewsToggle" checked="checked" type="checkbox">
+                <label for="cropimagepreviewsToggle"><?php p($l->t('Crop image previews')); ?></label>
+            </div>
+            <!-- <label for="webdavurl"><?php p($l->t('WebDAV')); ?></label>
 			<input id="webdavurl" type="text" readonly="readonly"
 				   value="<?php p($_['webdav_url']); ?>"/>
 			<em><a href="<?php echo link_to_docs('user-webdav') ?>" target="_blank" rel="noreferrer noopener"><?php p($l->t('Use this address to access your Files via WebDAV')) ?> ↗</a></em> -->
-		</div>
-	</div>
+        </div>
+    </div>
 
 
 </div>
@@ -101,40 +103,39 @@ script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
  */
 function NavigationListElements($item, $l, $pinned) {
 	strpos($item['classes'] ?? '', 'pinned') !== false ? $pinned++ : ''; ?>
-	<li
-		data-id="<?php p($item['id']) ?>"
-		<?php if (isset($item['dir'])) { ?> data-dir="<?php p($item['dir']); ?>" <?php } ?>
-		<?php if (isset($item['view'])) { ?> data-view="<?php p($item['view']); ?>" <?php } ?>
-		<?php if (isset($item['expandedState'])) { ?> data-expandedstate="<?php p($item['expandedState']); ?>" <?php } ?>
-		class="nav-<?php p($item['id']) ?>
+<li data-id="<?php p($item['id']) ?>" <?php if (isset($item['dir'])) { ?> data-dir="<?php p($item['dir']); ?>"
+    <?php } ?> <?php if (isset($item['view'])) { ?> data-view="<?php p($item['view']); ?>" <?php } ?>
+    <?php if (isset($item['expandedState'])) { ?> data-expandedstate="<?php p($item['expandedState']); ?>" <?php } ?>
+    class="nav-<?php p($item['id']) ?>
 		<?php if (isset($item['classes'])) {
 		p($item['classes']);
 	} ?>
 		<?php p($pinned === 1 ? 'first-pinned' : '') ?>
 		<?php if (isset($item['defaultExpandedState']) && $item['defaultExpandedState']) { ?> open<?php } ?>"
-		<?php if (isset($item['folderPosition'])) { ?> folderposition="<?php p($item['folderPosition']); ?>" <?php } ?>>
+    <?php if (isset($item['folderPosition'])) { ?> folderposition="<?php p($item['folderPosition']); ?>" <?php } ?>>
 
-		<a href="<?php p(isset($item['href']) ? $item['href'] : '#') ?>"
-		   class="nav-icon-<?php p(isset($item['icon']) && $item['icon'] !== '' ? $item['icon'] : $item['id']) ?> svg"><?php p($item['name']); ?></a>
+    <a href="<?php p(isset($item['href']) ? $item['href'] : '#') ?>"
+        class="nav-icon-<?php p(isset($item['icon']) && $item['icon'] !== '' ? $item['icon'] : $item['id']) ?> svg"><?php p($item['name']); ?></a>
 
 
-		<?php
+    <?php
 		NavigationElementMenu($item);
 	if (isset($item['sublist'])) {
 		?>
-			<button class="collapse app-navigation-noclose" <?php if (sizeof($item['sublist']) == 0) { ?> style="display: none" <?php } ?>></button>
-			<ul id="sublist-<?php p($item['id']); ?>">
-				<?php
+    <button class="collapse app-navigation-noclose" <?php if (sizeof($item['sublist']) == 0) { ?> style="display: none"
+        <?php } ?>></button>
+    <ul id="sublist-<?php p($item['id']); ?>">
+        <?php
 				foreach ($item['sublist'] as $item) {
 					$pinned = NavigationListElements($item, $l, $pinned);
 				} ?>
-			</ul>
-		<?php
+    </ul>
+    <?php
 	} ?>
-	</li>
+</li>
 
 
-	<?php
+<?php
 	return $pinned;
 }
 
@@ -148,20 +149,20 @@ function NavigationListElements($item, $l, $pinned) {
 function NavigationElementMenu($item) {
 	if (isset($item['menubuttons']) && $item['menubuttons'] === 'true') {
 		?>
-		<div id="dotmenu-<?php p($item['id']); ?>"
-			 class="app-navigation-entry-utils" <?php if (isset($item['enableMenuButton']) && $item['enableMenuButton'] === 0) { ?> style="display: none"<?php } ?>>
-			<ul>
-				<li class="app-navigation-entry-utils-menu-button svg">
-					<button id="dotmenu-button-<?php p($item['id']) ?>"></button>
-				</li>
-			</ul>
-		</div>
-		<div id="dotmenu-content-<?php p($item['id']) ?>"
-			 class="app-navigation-entry-menu">
-			<ul>
+<div id="dotmenu-<?php p($item['id']); ?>" class="app-navigation-entry-utils"
+    <?php if (isset($item['enableMenuButton']) && $item['enableMenuButton'] === 0) { ?> style="display: none"
+    <?php } ?>>
+    <ul>
+        <li class="app-navigation-entry-utils-menu-button svg">
+            <button id="dotmenu-button-<?php p($item['id']) ?>"></button>
+        </li>
+    </ul>
+</div>
+<div id="dotmenu-content-<?php p($item['id']) ?>" class="app-navigation-entry-menu">
+    <ul>
 
-			</ul>
-		</div>
-	<?php
+    </ul>
+</div>
+<?php
 	}
 }
