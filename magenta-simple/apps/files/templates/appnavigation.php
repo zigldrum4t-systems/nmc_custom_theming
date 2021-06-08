03f2a49b4b4689216d0e90319e20b7f9205a79ce
Desai <Sangramsinh.Desai@t-systems.com>
2021-06-08 19:39:43 +0530

    Storage text seperated - Sangram Desai

--- magenta-simple/apps/files/templates/appnavigation.php
+++ magenta-simple/apps/files/templates/appnavigation.php
@@ -49,8 +49,8 @@ script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
     </div> -->
 			
     <div class="Memory-consumed pinned <?php p($pinned === 0 ? 'first-pinned ' : '') ?>" title="<?php p($l->t('%s%% of %s used', [$_['usage_relative'], $_['total_space']])); ?>">
-		<div class="left-logo"><img src="#"></div>
-		<div class="logo-right-text"><strong><?php p($l->t('%1$s out of  %2$s','')); ?></strong> <?php p('',$l->t([$_['usage'], $_['total_space']])); ?></div>
+		<div class="left-logo"><img src='C:/Magenta cloud/themes/magenta-simple/core/img/CloudPink.png'></div>
+		<div class="logo-right-text"><?php p($l->t('%1$s out of  %2$s', [$_['usage'], $_['total_space']])); ?></div>
     </div>
 	
 
