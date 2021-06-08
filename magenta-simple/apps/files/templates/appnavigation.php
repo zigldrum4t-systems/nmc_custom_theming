01bda340af70ae25cfb4327d934fbc08ee25abf4
Desai <Sangramsinh.Desai@t-systems.com>
2021-06-08 18:36:02 +0530

    memory used and total stoarge in left menu bar added dynamic-Sangram Desai

--- magenta-simple/apps/files/templates/appnavigation.php
+++ magenta-simple/apps/files/templates/appnavigation.php
@@ -26,23 +26,18 @@ script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
 
     </ul>
 
-    <div class="memoryused">
+    <!-- <div class="memoryused">
         <?php if ($_['quota'] === \OCP\Files\FileInfo::SPACE_UNLIMITED): ?>
         <div id="quota" class="pinned <?php p($pinned === 0 ? 'first-pinned ' : '') ?>">
             <a href="#" class="icon-image">
-                <img src='#'>
-			   <p id="quotatext"><?php p($l->t('%1$s of %2$s used', [$_['usage'], $_['total_space']])); ?></p>
-			   <div class="quota-container">
-                    <progress value="<?php p($_['usage_relative']); ?>" max="100"
-                        class="<?= ($_['usage_relative'] > 80) ? 'warn' : '' ?>"></progress>
-			    </div>       
-			</a>
+                <img src='C:/Magenta cloud/themes/custom-theme/core/img/favicon.png'>
+                <p class="memorytext"><?php p($l->t('%s used', [$_['usage']])); ?></p>
+            </a>
         </div>
         <?php else: ?>
         <div id="quota" class="has-tooltip pinned <?php p($pinned === 0 ? 'first-pinned ' : '') ?>"
             title="<?php p($l->t('%s%% of %s used', [$_['usage_relative'], $_['total_space']])); ?>">
             <a href="#" class="icon-quota svg">
-				<img src='#'>
                 <p id="quotatext"><?php p($l->t('%1$s of %2$s used', [$_['usage'], $_['total_space']])); ?></p>
                 <div class="quota-container">
                     <progress value="<?php p($_['usage_relative']); ?>" max="100"
@@ -51,15 +46,14 @@ script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
             </a>
         </div>
         <?php endif; ?>
-    </div> 
-	<!--		
+    </div> -->
+			
     <div class="Memory-consumed">
 		<div class="left-logo"><img src='C:/Magenta cloud/themes/magenta-simple/core/img/CloudPink.png'></div>
-		<div class="logo-right-text"><span>5 MB Used</span>, von 25 GB</div>
+		<div class="logo-right-text"><?php p($l->t('%1$s used,  %2$s used', [$_['usage'], $_['total_space']])); ?>/div>
     </div>
-
-
 	
+
     <div class="NextCloudPorgressBar">
         <div class="progress customprogressbar">
             <div class="progress-bar styledbar" role="progressbar" aria-valuenow="60" aria-valuemin="0"
@@ -68,7 +62,7 @@ script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
             </div>
         </div>
 
-		-->	
+
         <!-- <li><span class="bar"><span class="style-html"></span></span></li> -->
     </div>
     <div class="custom-button">
