2e0549c3c88b51e104f8ccba6f5525172fc60c06
EMEA2\A94860742 <mankarandeep.saini@t-systems.com>
2021-06-08 12:34:08 +0530

    Refined code-MANKARAN

--- magenta-simple/apps/files/templates/appnavigation.php
+++ magenta-simple/apps/files/templates/appnavigation.php
@@ -47,8 +47,8 @@ script(\OCA\Files\AppInfo\Application::APP_ID, 'dist/files-app-settings');
 			<?php endif; ?>
 	</div>
 
-	  <div class="progress .customprogressbar">
-       <div class="progress-bar .styledbar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+	  <div class="progress customprogressbar">
+       <div class="progress-bar styledbar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
        <span class="sr-only">60% Complete</span>
      </div>
 
