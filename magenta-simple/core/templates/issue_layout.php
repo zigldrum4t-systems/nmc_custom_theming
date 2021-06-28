f475e80e5f16afb5ee9e629a0ca8f68cb1084a30
Desai <Sangramsinh.Desai@t-systems.com>
2021-06-28 10:15:19 +0530

    Share popup issue not opening issue fixed-Sangram

--- magenta-simple/core/templates/issue_layout.php
+++ magenta-simple/core/templates/issue_layout.php
@@ -37,6 +37,50 @@
     <?php print_unescaped($_['headers']); ?>
 </head>
 
+<body id="<?php p($_['bodyid']);?>">
+    <?php include 'layout.noscript.warning.php'; ?>
+
+    <?php foreach ($_['initialStates'] as $app => $initialState) { ?>
+    <input type="hidden" id="initial-state-<?php p($app); ?>" value="<?php p(base64_encode($initialState)); ?>">
+    <!DOCTYPE html>
+<html class="ng-csp" data-placeholder-focus="false" lang="<?php p($_['language']); ?>"
+    data-locale="<?php p($_['locale']); ?>">
+
+<head data-user="<?php p($_['user_uid']); ?>" data-user-displayname="<?php p($_['user_displayname']); ?>"
+    data-requesttoken="<?php p($_['requesttoken']); ?>">
+
+    <meta charset="utf-8">
+
+    <title>
+        <?php
+				p(!empty($_['application'])?$_['application'].' - ':'');
+				p($theme->getTitle());
+			?>
+    </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <?php if ($theme->getiTunesAppId() !== '') { ?>
+    <meta name="apple-itunes-app" content="app-id=<?php p($theme->getiTunesAppId()); ?>">
+    <?php } ?>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title"
+        content="<?php p((!empty($_['application']) && $_['appid'] != 'files')? $_['application']:$theme->getTitle()); ?>">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="<?php p($theme->getColorPrimary()); ?>">
+    <link rel="icon" href="<?php print_unescaped(image_path($_['appid'], 'favicon.ico')); /* IE11+ supports png */ ?>">
+    <link rel="apple-touch-icon" href="<?php print_unescaped(image_path($_['appid'], 'favicon-touch.png')); ?>">
+    <link rel="apple-touch-icon-precomposed"
+        href="<?php print_unescaped(image_path($_['appid'], 'favicon-touch.png')); ?>">
+    <link rel="mask-icon" sizes="any" href="<?php print_unescaped(image_path($_['appid'], 'favicon-mask.svg')); ?>"
+        color="<?php p($theme->getColorPrimary()); ?>">
+    <link rel="manifest" href="<?php print_unescaped(image_path($_['appid'], 'manifest.json')); ?>">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <?php emit_css_loading_tags($_); ?>
+    <?php emit_script_loading_tags($_); ?>
+    <?php print_unescaped($_['headers']); ?>
+</head>
+
 
 <body id="<?php p($_['bodyid']);?>">
     <?php include 'layout.noscript.warning.php'; ?>
@@ -77,6 +121,7 @@
 
         <header role="banner" id="header" class="header-bar">
             <div class="container-fixed">
+
                 <div class="header-left">
                     <div class="logo-area">
                         <div class="logo-area__inner">
@@ -139,10 +184,13 @@
                             </div>
                         </div>
                     </nav>
+
                 </div>
 
                 <div class="header-right">
+
                     <div class="navbar-icons">
+
                         <scale-icon name="shopping-bag" />
                         <a href="#"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 16"
                                 height="16" width="16">
@@ -158,12 +206,58 @@
                                 <path d="m8 1l-8 8h3v6h10v-6h3l-3-3v-4h-3v1l-2-2z" />
                             </svg><label>Menu</label></a>
                     </div>
+
+
                 </div>
+                <!-- <div id="unified-search"></div>
+                <div class="unified-search-title">Search</div>
+                <div id="contactsmenu">
+                    <div class="icon-contacts menutoggle" tabindex="0" role="button" aria-haspopup="true"
+                        aria-controls="contactsmenu-menu" aria-expanded="false">
+                        <span class="hidden-visually"><?php p($l->t('Contacts'));?></span>
+                    </div>
+                    <div id="contactsmenu-menu" class="menu" aria-label="<?php p($l->t('Contacts menu'));?>"></div>
+                </div>
+                <div class="contactsmenu-title">Email</div>
+
+                <div id="settings">
+                    <div id="expand" tabindex="0" role="button" class="menutoggle"
+                        aria-label="<?php p($l->t('Settings'));?>" aria-haspopup="true" aria-controls="expanddiv"
+                        aria-expanded="false">
+                        <div class="avatardiv<?php if ($_['userAvatarSet']) {
+				         print_unescaped(' avatardiv-shown');
+			              } else {
+				            print_unescaped('" style="display: none');
+			                 } ?>">
+                            <?php if ($_['userAvatarSet']): ?>
+                            <div class="icon-user-menu"></div>
+                            <div class="user-id-title"><?php p($_['user_uid']); ?></div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                        
+                </div>
+                    <nav class="settings-menu" id="expanddiv" style="display:none;"
+                        aria-label="<?php p($l->t('Settings menu'));?>">
+                        <ul>
+                            <?php foreach ($_['settingsnavigation'] as $entry):?>
+                            <li data-id="<?php p($entry['id']); ?>">
+                                <a href="<?php print_unescaped($entry['href']); ?>" <?php if ($entry["active"]): ?>
+                                    class="active" <?php endif; ?>>
+                                    <img alt=""
+                                        src="<?php print_unescaped($entry['icon'] . '?v=' . $_['versionHash']); ?>">
+                                    <?php p($entry['name']) ?>
+                                </a>
+                            </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </nav> -->
+
             </div>
         </header>
-    </div>
-    <div class="breadcrumb-bar">
-        <div class="container-fixed">
+         
+        <div class="breadcrumb-bar">
+         <div class="container-fixed">
             <div class="col-xs-3 left-part"></div>
             <div class="col-xs-9 right-part">
                 <ul class="breadcrumb-style">
@@ -177,27 +271,28 @@
                     <li><a href="#">Scans</a></li>
                 </ul>
             </div>
+         </div>
         </div>
-    </div>
 
-    <div id="sudo-login-background" class="hidden"></div>
-    <form id="sudo-login-form" class="hidden" method="POST">
-        <label>
+
+       <div id="sudo-login-background" class="hidden"></div>
+       <form id="sudo-login-form" class="hidden" method="POST">
+         <label>
             <?php p($l->t('This action requires you to confirm your password')); ?><br />
             <input type="password" class="question" autocomplete="new-password" name="question"
                 value=" <?php /* Hack against browsers ignoring autocomplete="off" */ ?>"
                 placeholder="<?php p($l->t('Confirm your password')); ?>" />
-        </label>
+         </label>
         <input class="confirm" value="<?php p($l->t('Confirm')); ?>" type="submit">
-    </form>
+       </form>
 
-    <div id="content" class="app-<?php p($_['appid']) ?>" role="main">
+       <div id="content" class="app-<?php p($_['appid']) ?>" role="main">
         <div class="container-fixed">
             <?php print_unescaped($_['content']); ?>
         </div>
-    </div>
+       </div>
 
-    <footer class="brand-footer">
+       <footer class="brand-footer">
         <div class="container-fixed">
             <div class="row brand-footer-bar">
                 <div class="col-l-4 col-s-12 text-muted">
@@ -215,7 +310,9 @@
                 </div>
             </div>
         </div>
-    </footer>
+       </footer>
+    </div>
+    
 </body>
 
 </html>
