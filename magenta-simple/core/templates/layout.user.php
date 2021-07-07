7189b7760dd963b5075ca39bc9e56cc65b99a9ea
Desai <Sangramsinh.Desai@t-systems.com>
2021-07-07 16:04:05 +0530

    header right search ico text added-Sangram

--- magenta-simple/core/templates/layout.user.php
+++ magenta-simple/core/templates/layout.user.php
@@ -146,7 +146,7 @@
                 </div>
 
                 <div class="header-right">
-                    <div id="unified-search"><label>Search</label></div>
+                    <div id="unified-search"><label></label><span><?php p($l->t('Search'));?></span></div>
                     <div id="contactsmenu">
                         <div class="menutoggle" tabindex="0" role="button" aria-haspopup="true"
                             aria-controls="contactsmenu-menu" aria-expanded="false">
