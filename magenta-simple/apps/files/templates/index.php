96b4381e62616a9c7a59fad8f203b11d77856c8e
Desai <Sangramsinh.Desai@t-systems.com>
2021-07-10 09:10:38 +0530

    list.php fi;e updated-Sangram

--- magenta-simple/apps/files/templates/index.php
+++ magenta-simple/apps/files/templates/index.php
@@ -1,6 +1,19 @@
 <?php /** @var \OCP\IL10N $l */ ?>
-<?php $_['appNavigation']->printPage(); ?>
-<div id="app-content">
+	<div id="controls">
+		<div class="actions creatable hidden">
+			<div id="uploadprogresswrapper">
+			</div>
+		</div>
+		<div id="file_action_panel"></div>
+		<div class="notCreatable notPublic hidden">
+			<?php p($l->t('You don’t have permission to upload or create files here'))?>
+		</div>
+		<?php /* Note: the template attributes are here only for the public page. These are normally loaded
++			 through ajax instead (updateStorageStatistics).
++	*/ ?>
+	</div>
+ <?php $_['appNavigation']->printPage(); ?>
+ <div id="app-content">
 
 	<?php if (!$_['isIE']) { ?>
 		<input type="checkbox" class="hidden-visually" id="showgridview"
