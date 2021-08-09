750ab2e565c9da59e54cfb18a497141f921aa138
naresh rai <naresh.rai@t-systems.com>
2021-08-09 13:56:00 +0530

    bredcrumb bar issue resolved

--- magenta-simple/core/js/custom.js
+++ magenta-simple/core/js/custom.js
@@ -8,9 +8,8 @@ function brandBarAnimation() {
         document.getElementsByClassName('logo-area__inner')[0].classList.add('brandbar-logo-minified');
         document.getElementsByClassName('brandbar')[0].classList.add('brand-bar-translation');
         document.getElementById('controls').classList.add('controls-translation', 'translation');
-        document.getElementById('view-toggle').classList.add('view-toggle-translation');
         document.getElementById('app-navigation-toggle').classList.add('app-navigation-translation');
-
+        document.getElementsByClassName('full-width-breadcrumb')[0].classList.add('breadcrumb-translation');
     }, 3000);
     // let workSpaceelement = document.getElementById('showRichWorkspacesToggle');
     // let workSpaceevent = new Event('change');
