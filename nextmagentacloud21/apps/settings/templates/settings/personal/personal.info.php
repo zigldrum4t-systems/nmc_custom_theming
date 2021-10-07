25e36aacdc5173c7cd5140de9375f4dcd78c06e8
mankarandeep.saini@t-systems.com <mankarandeep.saini@t-systems.com>
2021-10-07 11:40:04 +0000

    personal info ui changes

--- nextmagentacloud21/apps/settings/templates/settings/personal/personal.info.php
+++ nextmagentacloud21/apps/settings/templates/settings/personal/personal.info.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright Copyright (c) 2017 Arthur Schiwon <blizzz@arthur-schiwon.de>
  *
@@ -26,162 +27,220 @@
 /** @var array $_ */
 
 script('settings', [
-	'usersettings',
-	'templates',
-	'federationsettingsview',
-	'federationscopemenu',
-	'settings/personalInfo',
+  'usersettings',
+  'templates',
+  'federationsettingsview',
+  'federationscopemenu',
+  'settings/personalInfo',
 ]);
 ?>
- <div>
-    <div class="setting-flex-container">
-      <div class="setting-fullname-div">
-        <div class="personal-settings-setting-box">
-          <form id="displaynameform" class="section">
-            <h3>
-              <label for="displayname"><?php p($l->t('Full name')); ?></label>
-              <a href="#" class="federation-menu" aria-label="<?php p($l->t('Change privacy level of full name')); ?>">
-                <span class="icon-federation-menu icon-password">
-                  <span class="icon-triangle-s"></span>
-                </span>
-              </a>
-            </h3>
-            <input type="text" id="displayname" name="displayname"
-              <?php if (!$_['displayNameChangeSupported']) {
-                      print_unescaped('class="hidden"');
-                    } ?>
-                  value="<?php p($_['displayName']) ?>"
-                  autocomplete="on" autocapitalize="none" autocorrect="off" />
-            <?php if (!$_['displayNameChangeSupported']) { ?>
-              <span><?php if (isset($_['displayName']) && !empty($_['displayName'])) {
-                      p($_['displayName']);
-                    } else {
-                      p($l->t('No display name set'));
-                    } ?></span>
-            <?php } ?>
-            <span class="icon-checkmark hidden"></span>
-            <span class="icon-error hidden" ></span>
-            <input type="hidden" id="displaynamescope" value="<?php p($_['displayNameScope']) ?>">
-          </form>
-        </div>
-      </div>
-      <div class="setting-email-div">
-        <div class="personal-settings-setting-box">
-          <form id="emailform" class="section">
-            <h3>
-              <label for="email"><?php p($l->t('Email')); ?></label>
-              <a href="#" class="federation-menu" aria-label="<?php p($l->t('Change privacy level of email')); ?>">
-                <span class="icon-federation-menu icon-password">
-                  <span class="icon-triangle-s"></span>
-                </span>
-              </a>
-            </h3>
-            <div class="verify <?php if ($_['email'] === '' || $_['emailScope'] !== 'public') {
-                      p('hidden');
-                    } ?>">
-              <img id="verify-email" title="<?php p($_['emailMessage']); ?>" data-status="<?php p($_['emailVerification']) ?>" src="
-            <?php
-              switch ($_['emailVerification']) {
-                case \OC\Accounts\AccountManager::VERIFICATION_IN_PROGRESS:
-                  p(image_path('core', 'actions/verifying.svg'));
-                  break;
-                case \OC\Accounts\AccountManager::VERIFIED:
-                  p(image_path('core', 'actions/verified.svg'));
-                  break;
-                default:
-                  p(image_path('core', 'actions/verify.svg'));
-              }
-              ?>">
-            </div>
-            <input type="email" name="email" id="email" value="<?php p($_['email']); ?>"
-              <?php if (!$_['displayNameChangeSupported']) {
-                print_unescaped('class="hidden"');
-              } ?>
-                  placeholder="<?php p($l->t('Your email address')); ?>"
-                  autocomplete="on" autocapitalize="none" autocorrect="off" />
-            <span class="icon-checkmark hidden"></span>
-            <span class="icon-error hidden" ></span>
-            <?php if (!$_['displayNameChangeSupported']) { ?>
-              <span><?php if (isset($_['email']) && !empty($_['email'])) {
-                p($_['email']);
-              } else {
-                p($l->t('No email address set'));
-              }?></span>
-            <?php } ?>
-            <?php if ($_['displayNameChangeSupported']) { ?>
-              <em><?php p($l->t('For password reset and notifications')); ?></em>
-            <?php } ?>
-            <input type="hidden" id="emailscope" value="<?php p($_['emailScope']) ?>">
-          </form>
-        </div>
-      </div>
-    <div>
 
-    <div>
-      <div class="profile-settings-container">
-        <div class="personal-settings-setting-box personal-settings-language-box">
-          <?php if (isset($_['activelanguage'])) { ?>
-            <form id="language" class="section">
-              <h3>
-                <label for="languageinput"><?php p($l->t('Language'));?></label>
-              </h3>
-              <select id="languageinput" name="lang" data-placeholder="<?php p($l->t('Language'));?>">
-                <option value="<?php p($_['activelanguage']['code']);?>">
-                  <?php p($_['activelanguage']['name']);?>
-                </option>
-                <?php foreach ($_['commonlanguages'] as $language):?>
-                  <?php if($language['code']=="de_DE" || $language['code']=="en_GB"){ ?>
-                    <option value="<?php p($language['code']);?>">
-                      <?php p($language['name']);?>
-                    </option>
-                  <?php } ?>
-                <?php endforeach;?>
-                <optgroup label="––––––––––"></optgroup>
-                <?php foreach ($_['languages'] as $language):?>
-                  <?php if($language['code']=="de_DE" || $language['code']=="en_GB"){ ?>
-                    <option value="<?php p($language['code']);?>">
-                      <?php p($language['name']);?>
-                    </option>
-                  <?php } ?>
-                <?php endforeach;?>
-              </select>
-              <a href="https://www.transifex.com/nextcloud/nextcloud/"
-                  target="_blank" rel="noreferrer noopener">
-                <em><?php p($l->t('Help translate'));?></em>
-              </a>
-            </form>
-          <?php } ?>
+<div id="personal-settings" data-lookup-server-upload-enabled="<?php p($_['lookupServerUploadEnabled'] ? 'true' : 'false') ?>">
+  <h2 class="hidden-visually"><?php p($l->t('Personal info')); ?></h2>
+  <div id="personal-settings-avatar-container" class="personal-settings-container">
+  <h3><?php p($l->t('Account details')); ?></h3>
+
+  <div class="personal-settings-container">
+    <div class="personal-settings-setting-box">
+      <form id="displaynameform" class="section">
+        <h3>
+          <label for="displayname"><?php p($l->t('Name')); ?></label>
+          <a href="#" class="federation-menu" aria-label="<?php p($l->t('Change privacy level of full name')); ?>">
+            <span class="icon-federation-menu icon-password">
+              <span class="icon-triangle-s"></span>
+            </span>
+          </a>
+        </h3>
+        <input type="text" id="displayname" name="displayname" <?php if (!$_['displayNameChangeSupported']) {
+                                                                  print_unescaped('class="hidden"');
+                                                                } ?> value="<?php p($_['displayName']) ?>" autocomplete="on" autocapitalize="none" autocorrect="off" />
+        <?php if (!$_['displayNameChangeSupported']) { ?>
+          <span><?php if (isset($_['displayName']) && !empty($_['displayName'])) {
+                  p($_['displayName']);
+                } else {
+                  p($l->t('No display name set'));
+                } ?></span>
+        <?php } ?>
+        <span class="icon-checkmark hidden"></span>
+        <span class="icon-error hidden"></span>
+        <input type="hidden" id="displaynamescope" value="<?php p($_['displayNameScope']) ?>">
+      </form>
+    </div>
+    <div class="personal-settings-setting-box">
+      <form id="emailform" class="section">
+        <h3>
+          <label for="email"><?php p($l->t('Mail address')); ?></label>
+          <a href="#" class="federation-menu" aria-label="<?php p($l->t('Change privacy level of email')); ?>">
+            <span class="icon-federation-menu icon-password">
+              <span class="icon-triangle-s"></span>
+            </span>
+          </a>
+        </h3>
+        <div class="verify <?php if ($_['email'] === '' || $_['emailScope'] !== 'public') {
+                              p('hidden');
+                            } ?>">
+          <img id="verify-email" title="<?php p($_['emailMessage']); ?>" data-status="<?php p($_['emailVerification']) ?>" src="
+				<?php
+        switch ($_['emailVerification']) {
+          case \OC\Accounts\AccountManager::VERIFICATION_IN_PROGRESS:
+            p(image_path('core', 'actions/verifying.svg'));
+            break;
+          case \OC\Accounts\AccountManager::VERIFIED:
+            p(image_path('core', 'actions/verified.svg'));
+            break;
+          default:
+            p(image_path('core', 'actions/verify.svg'));
+        }
+        ?>">
         </div>
-      <span class="msg"></span>
-      </div>
+        <input type="email" name="email" id="email" value="<?php p($_['email']); ?>" <?php if (!$_['displayNameChangeSupported']) {
+                                                                                        print_unescaped('class="hidden"');
+                                                                                      } ?> placeholder="<?php p($l->t('Your email address')); ?>" autocomplete="on" autocapitalize="none" autocorrect="off" />
+        <span class="icon-checkmark hidden"></span>
+        <span class="icon-error hidden"></span>
+        <?php if (!$_['displayNameChangeSupported']) { ?>
+          <span><?php if (isset($_['email']) && !empty($_['email'])) {
+                  p($_['email']);
+                } else {
+                  p($l->t('No email address set'));
+                } ?></span>
+        <?php } ?>
+        <?php if ($_['displayNameChangeSupported']) { ?>
+          <em><?php p($l->t('For password reset and notifications')); ?></em>
+        <?php } ?>
+        <input type="hidden" id="emailscope" value="<?php p($_['emailScope']) ?>">
+      </form>
+    </div>
+    <div class="profile-settings-container">
+    <div class="personal-settings-setting-box personal-settings-language-box">
+      <?php if (isset($_['activelanguage'])) { ?>
+        <form id="language" class="section">
+          <h3>
+            <label for="languageinput"><?php p($l->t('Language')); ?></label>
+          </h3>
+          <select id="languageinput" name="lang" data-placeholder="<?php p($l->t('Language')); ?>">
+            <option style="" value="<?php p($_['activelanguage']['code']); ?>">
+              <?php p($_['activelanguage']['name']); ?>
+            </option>
+            <optgroup label="––––––––––"></optgroup>
+            <?php foreach ($_['commonlanguages'] as $language) : ?>
+              <?php if ($language['code'] == "de_DE") { ?>
+                <option value="<?php p($language['code']); ?>">
+                  <?php p($language['name']); ?>
+                </option>
+              <?php } ?>
+            <?php endforeach; ?>
+
+            <?php foreach (array_unique($_['languages'], SORT_REGULAR) as $language) : ?>
+              <?php if ($language['code'] == "en_GB") { ?>
+                <option value="<?php p($language['code']); ?>">
+                  <?php p($language['name']); ?>
+                </option>
+              <?php } ?>
+            <?php endforeach; ?>
+
+
+          </select>
+          <a href="https://www.transifex.com/nextcloud/nextcloud/" target="_blank" rel="noreferrer noopener">
+            <em><?php p($l->t('Help translate')); ?></em>
+          </a>
+        </form>
+      <?php } ?>
     </div>
 
-<div>
-  <div class="personal-settings-setting-box personal-settings-group-box section">
-      <h3><?php p($l->t('Details')); ?></h3>
-      <div id="groups" class="personal-info icon-user">
-        <p><?php p($l->t('You are a member of the following groups:')); ?></p>
-        <p id="groups-groups">
-          <strong><?php p(implode(', ', $_['groups'])); ?></strong>
-        </p>
-      </div>
+    <span class="msg"></span>
+  </div>
+  </div>
+  <div class="telekom-link">
+  <p><label><?php p($l->t('You can change your password in the')); ?>
+  <a href='https://account.idm.telekom.com/account-manager/index.xhtml' target='_blank'>login settings</a>
+          <?php p($l->t('for all telekom services.')); ?>
+</label>
+</p>
+
+    <div class="personal-settings-setting-box personal-settings-group-box section">
+    <b><?php p($l->t('Storage utilisation  ')); ?></b>
       <div id="quota" class="personal-info icon-quota">
         <div class="quotatext-bg">
           <p class="quotatext">
-            <?php if ($_['quota'] === \OCP\Files\FileInfo::SPACE_UNLIMITED): ?>
-              <?php print_unescaped($l->t('You are using <strong>%s</strong>',
-                [$_['usage']]));?>
-            <?php else: ?>
-              <?php print_unescaped($l->t('You are using <strong>%1$s</strong> of <strong>%2$s</strong> (<strong>%3$s %%</strong>)',
-                [$_['usage'], $_['total_space'],  $_['usage_relative']]));?>
+            <?php if ($_['quota'] === \OCP\Files\FileInfo::SPACE_UNLIMITED) : ?>
+              <?php print_unescaped($l->t(
+                '<strong>%1$s</strong> of <strong>%2$s</strong> ',
+                [$_['usage'], $_['total_space'],  $_['usage_relative']]
+              )); ?>
+            <?php else : ?>
+              <?php print_unescaped($l->t(
+                '<strong>%1$s</strong> of <strong>%2$s</strong>',
+                [$_['usage'], $_['total_space'],  $_['usage_relative']]
+              )); ?>
             <?php endif ?>
           </p>
         </div>
-        <progress value="<?php p($_['usage_relative']); ?>" max="100"<?php if ($_['usage_relative'] > 80): ?> class="warn" <?php endif; ?>></progress>
+        <progress value="<?php p($_['usage_relative']); ?>" max="100" <?php if ($_['usage_relative'] > 80) : ?> class="warn" <?php endif; ?>></progress>
       </div>
+      <div class="extra-details">
+      <div>
+        <div id="files"></div>
+        <?php print_unescaped($l->t(
+                 'Files:<strong>%1$s</strong> ',
+                [$_['usage']]
+        )); ?>
+      </div>
+              <div>
+              <div id="photos"></div>   <?php print_unescaped($l->t(
+                'Photos & videos:<strong>%1$s</strong> ',
+                [$_['usage']]
+              )); ?></div>
+               <div>
+               <div id="backup"></div>  <?php print_unescaped($l->t(
+                'Live Backups:<strong>%1$s </strong> ',
+                [$_['usage']]
+              )); ?></div>
+               <div>
+               <div id="bin"></div> <?php print_unescaped($l->t(
+                'Recycle Bin:<strong>%1$s</strong>',
+                [$_['usage']]
+              )); ?></div>
     </div>
+<div>
+<?php print_unescaped($l->t(
+                'The recycle bin is automatically tide up.'
+              )); ?>
+</div>
+<div>
+<?php print_unescaped($l->t(
+                'Files that have been in the recycle bin for longer then 30 days are automatically deleted permanently and free up storage space.'
+              )); ?>
 </div>
 
-  <div>
+
+  </div>
 
 
+  <div id="tarrifInfo" class="personal-settings-tarrif personal-settings-tarrif-box">
+    <b><?php p($l->t('Tariff information')); ?></b>
+    <div><?php print_unescaped($l->t(
+                '<strong>Your tariff</strong>: %1$s ',
+                [$_['usage']]
+              )); ?>
+              </div>
+              <div><?php print_unescaped($l->t(
+                '<strong>Storage  </strong>: %1$s ',
+                [$_['usage']]
+              )); ?>
+              </div>
+              <div>
+                <button><?php print_unescaped($l->t(
+                'Expand storage',
+                [$_['usage']]
+              )); ?></button>
+    </div>
+  </div>
+
+
+
+  <div id="personal-settings-group-container">
+
+  </div>
+
+</div>
