20754e6d2cbeb1c88b869c520380722399cf4ef3
TSI-ashishsharma <sharma.a@t-systems.com>
2021-10-18 13:54:55 +0530

    feature-684-my-shares related changes

--- nextmagentacloud21/apps/files_sharing/js/dist/additionalScripts.js
+++ nextmagentacloud21/apps/files_sharing/js/dist/additionalScripts.js
@@ -659,14 +659,26 @@ __webpack_require__.r(__webpack_exports__);
     */
     _formatShareList: function _formatShareList(recipients) {
       var _parent = this;
+      var returnVal='';
+      var firstname='';
 
       recipients = _.toArray(recipients);
       recipients.sort(function (a, b) {
         return a.shareWithDisplayName.localeCompare(b.shareWithDisplayName);
       });
-      return $.map(recipients, function (recipient) {
-        return _parent._formatRemoteShare(recipient.shareWith, recipient.shareWithDisplayName, t('files_sharing', 'Shared with'));
-      });
+      //  $.map(recipients, function (recipient) {
+      //   return _parent._formatRemoteShare(firstname, returnVal, t('files_sharing', 'Shared with'));
+      // });
+
+      $.each(recipients, function(key,val) {   
+        firstname =   val.shareWith;
+        returnVal+= val.shareWithDisplayName+",";        
+    }); 
+    returnVal = returnVal.replace(/,\s*$/, "");
+
+    returnVal= _parent._formatRemoteShare(firstname, returnVal, t('files_sharing', 'Shared with'));              
+     console.log(returnVal);
+     return returnVal;
     },
 
     /**
