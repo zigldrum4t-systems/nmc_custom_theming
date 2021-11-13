cd817295c0c6ba222f1d1c40992e3bdf3bd293a8
naresh rai <naresh.rai@t-systems.com>
2021-11-13 12:28:20 +0530

    breadcrumb issues resolved

--- nextmagentacloud21/core/js/customutils.js
+++ nextmagentacloud21/core/js/customutils.js
@@ -121,7 +121,7 @@ function breadcrumbAddLabel() {
   var ele = document.querySelectorAll('#controls .actions.creatable a')[0];
   var labelElement = document.createElement('label');
       labelElement.className = 'add-label';
-      labelElement.innerText = t('app', 'Add');
+      labelElement.innerText = t('core', 'Add');
 
      if(ele){
        ele.appendChild(labelElement);
