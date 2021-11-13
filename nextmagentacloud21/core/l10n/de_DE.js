eba79b3786eaedbf340ad43585528ebf09111a85
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2021-11-13 15:42:42 +0530

    File pre conflict translation added

--- nextmagentacloud21/core/l10n/de_DE.js
+++ nextmagentacloud21/core/l10n/de_DE.js
@@ -425,6 +425,14 @@ OC.L10N.register(
     "Magentacloud XL": "Magentacloud XL",
     "Magentacloud XXL": "Magentacloud XXL",
     "Search files, folders or settings …": "Suchen Sie nach Dateien, Ordner oder Einstellungen...",
-    "Show folder info text": "Ordnerinfotext anzeigen"
+    "Show folder info text": "Ordnerinfotext anzeigen",
+    "File conflict" : "Dateikonflikt",
+    "The file nextcloud.log already exist in the location." : "Die Datei nextcloud.log existiert bereits am Speicherort.",
+    "Do you want to replace it with files you're moving?" : "Möchten Sie es durch Dateien ersetzen, die Sie verschieben?",
+    "Keep both files": "Behalte beide Dateien",
+    "More details" : "Mehr Details",
+    "Replace file" : "Datei ersetzen",
+    "The file {filename} already exist in the location." : "Die Datei {filename} existiert bereits am Speicherort.",
+    "The files already exist in the location." : "Die Dateien sind bereits am Speicherort vorhanden."
 },
 "nplurals=2; plural=(n != 1);");
