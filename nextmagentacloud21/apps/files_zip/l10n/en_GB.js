549f56df60b77ec07d7578b79fc1cfa74d79773f
kavita.sonawane@t-systems.com <kavita.sonawane@t-systems.com>
2021-12-02 12:18:26 +0000

    Feature bug fixes file action searchbox

---
+++ nextmagentacloud21/apps/files_zip/l10n/en_GB.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "files_zip",
+    {
+    "Compress to zip": "Compress to zip",
+    "Archive": "Archive",
+    "Select a name for the zip archive": "Select a name for the zip archive",
+    "Compress files": "Compress files",
+    "File name": "File name",
+    "Cancel": "Cancel"
+},
+"nplurals=2; plural=(n != 1);");
