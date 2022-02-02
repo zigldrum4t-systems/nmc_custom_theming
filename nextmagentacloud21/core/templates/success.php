27bfcfa02ca5b55e23ed11822203c6cc63defb59
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2022-02-02 17:39:24 +0530

    Email confirmation and success page changes added

---
+++ nextmagentacloud21/core/templates/success.php
@@ -0,0 +1,14 @@
+<?php
+/** @var array $_ */
+/** @var \OCP\IL10N $l */
+/** @var \OCP\Defaults $theme */
+style('core', 'email/success');
+?>
+
+<div class="update">
+	<h2><?php p($_['title']) ?></h2>
+	<p><?php p($_['message']) ?></p>
+	<p><a class="button primary success-btn" href="<?php p(\OC::$server->get(\OCP\IURLGenerator::class)->linkTo('', 'index.php')) ?>">
+		<?php p($l->t('Go to %s', [$theme->getName()])); ?>
+	</a></p>
+</div>
