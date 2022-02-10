f981eb1e16eecb200b05bd15708cf6003cf8ee9d
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2022-02-10 15:14:02 +0530

    Verifcation page and success page changes updated

--- nextmagentacloud21/core/templates/success.php
+++ nextmagentacloud21/core/templates/success.php
@@ -2,7 +2,7 @@
 /** @var array $_ */
 /** @var \OCP\IL10N $l */
 /** @var \OCP\Defaults $theme */
-style('core', 'email/success');
+style('core', 'email/verification-page');
 ?>
 
 <div class="update">
