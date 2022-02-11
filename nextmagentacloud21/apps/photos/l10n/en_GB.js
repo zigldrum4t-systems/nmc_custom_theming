bf03577efc614a39f1d3be55564d9fc9f1031099
TSI-ashishsharma <sharma.a@t-systems.com>
2022-02-11 16:34:01 +0530

    language translation

--- nextmagentacloud21/apps/photos/l10n/en_GB.js
+++ nextmagentacloud21/apps/photos/l10n/en_GB.js
@@ -4,6 +4,9 @@ OC.L10N.register(
     "Photos" : "Photos",
     "Favorites" : "Favourites",
     "Shared with you" : "Shared with you",
-    "Settings" : "Settings"
+    "Settings" : "Settings",
+    "Folders":"Folders",
+    "Files":"Files",
+    "Close navigation" : "Hide menu"
 },
 "nplurals=2; plural=(n != 1);");
