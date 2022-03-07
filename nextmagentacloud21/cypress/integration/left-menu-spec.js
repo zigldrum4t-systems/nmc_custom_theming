e6d0e7ce27e08c799ec003bc054686c4a6fc5095
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2022-03-07 16:43:34 +0530

    left menu all links and storage button test cases updated

--- nextmagentacloud21/cypress/integration/left-menu-spec.js
+++ nextmagentacloud21/cypress/integration/left-menu-spec.js
@@ -1,39 +1,47 @@
 describe('Settings related changes', () => {
   beforeEach(() => {
-    cy.visit('http://release2104.nextmagentacloud.com/index.php/apps/files/?dir=/&fileid=3143')
-    const username='test'
-    const password='admin123'
-    cy.get('.grouptop input').type(`${username}{enter}`)
-    cy.get('.groupbottom input').type(`${password}{enter}`)
+    cy.visit(`${Cypress.env('local').app_url}/apps/files/`);
+    cy.get('.grouptop input').type(`${Cypress.env('local').user}{enter}`)
+    cy.get('.groupbottom input').type(`${Cypress.env('local').password}{enter}`)
   })
 
-  it('Left menu- Activities menu name in english', () => {
+  it('Left menu names change in english', () => {
+    cy.wait(3000)
+    cy.get('.settingsdiv').click()
+    cy.wait(1000)
+    cy.get('a[href*="/index.php/settings/user"]').contains('Einstellungen').click()
+    cy.wait(2000)
+    cy.get('#languageinput').select('English')
+    cy.wait(2000)
+    cy.visit(`${Cypress.env('local').app_url}/apps/files/`);
+    cy.wait(1000)
     cy.get("li[data-id='nmc_files_activity'] a").should('have.text','Activities')
-  })
-
-  it('Left menu- Favourites menu name in english', () => {
     cy.get("#app-navigation li[data-id='favorites'] a").should('have.text','Favourites')
-  })
-
-  it('Left menu- Files menu name in english', () => {
     cy.get("#app-navigation li[data-id='files'] a").should('have.text','All files')
-  })
-
-  it('Left menu- My shares menu name in english', () => {
     cy.get("#app-navigation li[data-id='sharingout'] a").should('have.text','My shares')
-  })
-
-  it('Left menu- Shared with me menu name in english', () => {
     cy.get("#app-navigation li[data-id='sharingin'] a").should('have.text','Shared with me')
-  })
-
-  it('Left menu- Shared with me menu name in english', () => {
     cy.get("#app-navigation li[data-id='trashbin'] a").should('have.text','Deleted files')
-  })
-
-  it('Left menu- Expand storage button text in english', () => {
     cy.get('.custom-button .btn-default').should('have.text',' Expand storage')
   })
 
+  it('Left menu names - in German', () => {
+    cy.wait(3000)
+    cy.get('.settingsdiv').click()
+    cy.wait(1000)
+    cy.get('a[href*="/index.php/settings/user"]').contains('Settings').click()
+    cy.wait(2000)
+    cy.get('#languageinput').select('Deutsch')
+    cy.wait(2000)
+    cy.visit(`${Cypress.env('local').app_url}/apps/files/`);
+    cy.wait(1000)
+
+    cy.get("li[data-id='nmc_files_activity'] a").should('have.text','Aktivitäten')
+    cy.get("#app-navigation li[data-id='favorites'] a").should('have.text','Favoriten')
+    cy.get("#app-navigation li[data-id='files'] a").should('have.text','Alle Dateien')
+    cy.get("#app-navigation li[data-id='sharingout'] a").should('have.text','Meine geteilten Inhalte')
+    cy.get("#app-navigation li[data-id='sharingin'] a").should('have.text','Mit mir geteilt')
+    cy.get("#app-navigation li[data-id='trashbin'] a").should('have.text','Gelöschte Dateien')
+    cy.get('.custom-button .btn-default').should('have.text',' Speicherplatz erweitern')
+  })
 
 })
