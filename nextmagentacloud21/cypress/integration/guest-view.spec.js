b0fd8f05ccd9a2e96129faa32aa41db04b4d9977
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2022-03-11 12:28:27 +0530

    Guest view test cases added

--- nextmagentacloud21/cypress/integration/guest-view.spec.js
+++ nextmagentacloud21/cypress/integration/guest-view.spec.js
@@ -16,6 +16,7 @@ describe('Settings related changes', () => {
     cy.visit(`${Cypress.env('local').app_url}/s/qtM34GLiesgJoRy`);
     cy.wait(1000)
     cy.get('#downloadFile').should('have.contain',"Download")
+    cy.get('.informNow').find(".text").should('have.contain',"Inform Now")
   })
 
 
@@ -29,14 +30,22 @@ describe('Settings related changes', () => {
     cy.visit(`${Cypress.env('local').app_url}/s/qtM34GLiesgJoRy`);
     cy.wait(1000)
     cy.get('#downloadFile').should('have.contain',"Herunterladen")
-
+    cy.get('.informNow').find(".text").should('have.contain',"Jetzt informieren")
   })
 
-  /*
-  it('Check Css for delete activity button', () => {
-    cy.get('.directDownload').find('#downloadFile').should('have.css', 'margin-top', '').should('have.css', 'margin-bottom', '40px').should('have.css', 'border-radius', '8px').should('have.css', 'color', 'rgb(255, 255, 255)');
+  it('Check Css for guest view', () => {
+    cy.visit(`${Cypress.env('local').app_url}/s/qtM34GLiesgJoRy`);
+    cy.get('#header-primary-action').find('.gust-download-svg').should('have.css', 'margin-right', '5px')
+    cy.get('#header-primary-action').find('.gust-download-label').should('have.css', 'font-size', '20px').should('have.css', 'font-weight', '700').should('have.css', 'vertical-align', 'super')
+    cy.get('.directDownload').find('.video-file-details').should('have.css', 'font-size', '20px')
+    cy.get('.directDownload').find('#downloadFile').should('have.css', 'margin-top', '16px').should('have.css', 'margin-bottom', '64px').should('have.css', 'border-radius', '8px').should('have.css','padding','10px 24px')
+    cy.get('.content-dialog').find('.content-para').should('have.css', 'max-width', '850px')
+    cy.get('.content-dialog').find('.informNow').should('have.css', 'margin-top', '24px')
+    cy.get('.content-dialog').find('.informNow').find('span').should('have.css', 'vertical-align', 'middle')
+    cy.get('.content-dialog').should('have.css', 'text-align', 'center').should('have.css', 'text-align','center').should('have.css','padding','24px')
   })
-  */
+
+
 
 
 
