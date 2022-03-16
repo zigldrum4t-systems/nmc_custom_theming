31b0a64eed0b18a65f0b3545b5d7b04d4fd41118
sangramsinh.desai@t-systems.com <sangramsinh.desai@t-systems.com>
2022-03-16 17:10:27 +0530

    Left menu test case updated

--- nextmagentacloud21/cypress/integration/left-menu-spec.js
+++ nextmagentacloud21/cypress/integration/left-menu-spec.js
@@ -5,6 +5,21 @@ describe('Settings related changes', () => {
     cy.get('.groupbottom input').type(`${Cypress.env('local').password}{enter}`)
   })
 
+  it('Check Css for delete activity button', () => {
+    cy.get('.app-navigation-translation').should('have.css', 'top', '73px')
+    cy.get('.NextCloudPorgressBar').find('.customprogressbar').should('have.css', 'height', '4px').should('have.css', 'border-radius', '4px').should('have.css', 'margin-bottom', '8px')
+    cy.get('.memory-ocup-message').should('have.css', 'font-size', '14px')
+    cy.get('#app-navigation').find('.custom-button').find('.btn-style').should('have.css', 'font-size', '16px')
+    .should('have.css', 'cursor', 'pointer').should('have.css', 'font-weight', '400')
+    .should('have.css', 'display', 'inline-block').should('have.css', 'font-family', 'TeleNeoWeb, TeleNeo, sans-serif')
+    cy.get('#app-settings-header').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+    cy.get('#app-settings-header').find('.settings-button').should('have.css', 'display', 'block').should('have.css', 'height', '20px').should('have.css', 'width', '287px')
+    .should('have.css', 'background-repeat', 'no-repeat').should('have.css', 'box-shadow', 'none').should('have.css', 'border', '0px none rgb(226, 0, 116)')
+    .should('have.css', 'border-radius', '0px').should('have.css', 'text-align', 'left').should('have.css', 'padding-left', '40px')
+    .should('have.css', 'font-weight', '400')
+  })
+
+
   it('Left menu names change in english', () => {
     cy.wait(3000)
     cy.get('.settingsdiv').click()
@@ -50,8 +65,6 @@ describe('Settings related changes', () => {
     cy.get("label[for='showhiddenfilesToggle']").should('have.contain','Versteckte Dateien anzeigen')
     cy.get("label[for='cropimagepreviewsToggle']").should('have.contain','Vorschaubilder  beschneiden')
   })
-
-
 
 })
 
