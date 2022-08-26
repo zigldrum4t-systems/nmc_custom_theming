f0ec93407c7dc40e3be0783f81252a5e284ad1b9
kavita.sonawane@t-systems.com <kavita.sonawane@t-systems.com>
2022-08-26 13:52:25 +0530

    Template changes

--- nextmagentacloud21/apps/files_trashbin/templates/index.php
+++ nextmagentacloud21/apps/files_trashbin/templates/index.php
@@ -1,36 +1,34 @@
-<?php
-
-/** @var \OCP\IL10N $l */ ?>
+<?php /** @var \OCP\IL10N $l */ ?>
 <div id="controls">
-  <div id="file_action_panel"></div>
+	<div id="file_action_panel"></div>
 </div>
 <div id='notification'></div>
 
 <div id="emptycontent" class="hidden">
-  <div class="icon-delete"></div>
-  <h2><?php p($l->t('No deleted files')); ?></h2>
-  <p><?php p($l->t('You will be able to recover deleted files from here')); ?></p>
+	<div class="icon-delete"></div>
+	<h2><?php p($l->t('No deleted files')); ?></h2>
+	<p><?php p($l->t('You will be able to recover deleted files from here')); ?></p>
 </div>
 
 <input type="hidden" name="dir" value="" id="dir">
 
-<div class="nofilterresults hidden">
-  <div class="icon-search"></div>
-  <h2><?php p($l->t('No entries found in this folder')); ?></h2>
-  <p></p>
+<div class="nofilterresults emptycontent hidden">
+	<div class="icon-search"></div>
+	<h2><?php p($l->t('No entries found in this folder')); ?></h2>
+	<p></p>
 </div>
 
 <table id="filestable" class="list-container <?php p($_['showgridview'] ? 'view-grid' : '') ?>">
-  <thead>
-    <tr>
-      <th id="headerSelection" class="hidden column-selection">
-        <input type="checkbox" id="select_all_trash" class="select-all checkbox" />
-        <label for="select_all_trash">
-          <span class="hidden-visually"><?php p($l->t('Select all')) ?></span>
-        </label>
-      </th>
+	<thead>
+		<tr>
+			<th id="headerSelection" class="hidden column-selection">
+				<input type="checkbox" id="select_all_trash" class="select-all checkbox"/>
+				<label for="select_all_trash">
+					<span class="hidden-visually"><?php p($l->t('Select all'))?></span>
+				</label>
+			</th>
       <th id='allLabel'><?php p($l->t('All')); ?></th>
-      <th id='headerName' class="hidden column-name">
+			<th id='headerName' class="hidden column-name">
           <div id="headerName-container">
 		      <a class="name sort columntitle" data-sort="name">
             <span><?php p($l->t('Name')); ?></span>
@@ -58,10 +56,10 @@
 					</a>
 				</span>
 			</th>
-    </tr>
-  </thead>
-  <tbody id="fileList">
-  </tbody>
-  <tfoot>
-  </tfoot>
+		</tr>
+	</thead>
+	<tbody id="fileList">
+	</tbody>
+	<tfoot>
+	</tfoot>
 </table>
