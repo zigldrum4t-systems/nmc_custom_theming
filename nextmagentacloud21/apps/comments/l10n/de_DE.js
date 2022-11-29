bfbc801582a900842837600ec27dc1773cd17eb9
kavita.sonawane@t-systems.com <kavita.sonawane@t-systems.com>
2022-11-29 13:42:19 +0530

    Comments related design changes and translation changes

---
+++ nextmagentacloud21/apps/comments/l10n/de_DE.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "comments",
+    {
+    "Write a message..." : "Nachricht schreiben...",
+    "No comments yet." : "Noch keine Kommentare.",
+    "You can comment on shared content. Your messages will reach everyone with whom the file or folder is shared." : "Sie können geteilte Inhalte kommentieren. Ihre Nachrichten werden alle erreichen, mit denen die Datei oder der Ordner geteilt ist."
+},
+"nplurals=2; plural=(n != 1);");
---
+++ nextmagentacloud21/apps/comments/l10n/en_GB.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "comments",
+    {
+    "Write a message..." : "Write a message...",
+    "No comments yet." : "No comments yet.",
+    "You can comment on shared content. Your messages will reach everyone with whom the file or folder is shared." : "You can comment on shared content. Your messages will reach everyone with whom the file or folder is shared."
+},
+"nplurals=2; plural=(n != 1);");
