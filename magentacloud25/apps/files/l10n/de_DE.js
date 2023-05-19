34df4e9057e35f8062a1ed6b06cca1a02c105f3e
kavita.sonawane@t-systems.com <kavita.sonawane@t-systems.com>
2023-05-19 18:37:59 +0530

    Toggle view translation

--- magentacloud25/apps/files/l10n/de_DE.js
+++ magentacloud25/apps/files/l10n/de_DE.js
@@ -2,6 +2,8 @@ OC.L10N.register(
     "files",
     {
     "All": "Alle",
-    "Cancel": "Abbrechen"
+    "Cancel": "Abbrechen",
+    "Show list view" : "Ansicht ändern",
+    "Show grid view" : "Ansicht ändern"
 },
 "nplurals=2; plural=(n != 1);");
--- magentacloud25/apps/files/l10n/en_GB.js
+++ magentacloud25/apps/files/l10n/en_GB.js
@@ -2,6 +2,8 @@ OC.L10N.register(
     "files",
     {
     "All": "All",
-	"Cancel": "Cancel"
+	"Cancel": "Cancel",
+    "Show list view" : "Toggle grid view",
+    "Show grid view" : "Toggle grid view"
 },
 "nplurals=2; plural=(n != 1);");
