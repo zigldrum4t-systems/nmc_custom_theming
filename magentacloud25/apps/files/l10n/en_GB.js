48356ef592bda29caf86c85c190aa7c12d9a0bfb
kavita.sonawane@t-systems.com <kavita.sonawane@t-systems.com>
2023-05-29 19:14:16 +0530

    Added translation for left menubar

--- magentacloud25/apps/files/l10n/en_GB.js
+++ magentacloud25/apps/files/l10n/en_GB.js
@@ -4,6 +4,11 @@ OC.L10N.register(
     "All": "All",
 	"Cancel": "Cancel",
     "Show list view" : "Toggle grid view",
-    "Show grid view" : "Toggle grid view"
+    "Show grid view" : "Toggle grid view",
+    "My shares": "My shares",
+    "Shared with me": "Shared with me",
+    "Display settings": "Display settings",
+    "Expand storage"  : "Expand storage",
+    "Memory used up to %s%%": "Memory used up to %s%%"
 },
 "nplurals=2; plural=(n != 1);");
