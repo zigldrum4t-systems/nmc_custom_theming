ae3ff73384a4e17b4eea47d6f81542183a331864
kavita.sonawane@t-systems.com <kavita.sonawane@t-systems.com>
2023-06-02 17:02:09 +0530

    Zipper related translation

---
+++ magentacloud25/apps/files_zip/l10n/de_DE.js
@@ -0,0 +1,16 @@
+OC.L10N.register(
+    "files_zip",
+    {
+    "Compress to Zip": "Zip-Datei erstellen",
+    "Archive": "Archiv",
+    "Select a name for the Zip archive": "Bitte wählen Sie einen Dateinamen für das Zip-Archiv",
+    "Compress files": "Archiv erstellen",
+    "File name": "Dateiname",
+    "Cancel": "Abbrechen",
+    "Compress {files} file": "{files} Dateien komprimieren",
+    "_Compress {files} file_::_Compress {files} files_" : ["{files} Element komprimieren","{files} Elemente komprimieren"],
+    "Creating Zip archive started. We will notify you as soon as the archive is available.": "Erstellen des Zip-Archivs gestartet. Die Dauer der Erstellung kann je nach Umfang variieren.",
+    "An error happened when trying to compress the file.": "Beim Komprimieren der Datei ist ein Fehler aufgetreten.",
+    "Your files have been stored as a Zip archive in {path}." : "Ihre Dateien wurden als Zip-Archiv in {path} gespeichert."
+},
+"nplurals=2; plural=(n != 1);");
